cdce42b905465e2a23f5c12800d24124a9ab835b
Biswajit Ghosh <ghoshbiswajit2406@gmail.com>
2022-08-04 12:01:41 +0530

    Product List Page

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -1,41 +1,73 @@
 import React from 'react'
 import styled from 'styled-components'
-import Announcement from '../Components/Announcement'
-import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
-import Newsletter from '../Components/Newsletter'
+import Anouncement from '../Components/Announcement'
 import Products from '../Components/Products'
+import NewsLetter from '../Components/Newsletter'
+import Footer from '../Components/Footer'
+
 const Container=styled.div``
 const Title=styled.h1`
-margin: 20px`
+margin: 20px;
+`
 const FilterContainer=styled.div`
 display: flex;
 justify-content: space-between;
 `
 const Filter=styled.div`
-margin:20px;
+display:flex;
+margin: 20px;
 `
 
 const FilterText=styled.span`
 font-size: 20px;
-font-weight:600;
+padding-top: 5px;
+font-weight: 600;
+margin-right: 20px;
 `
 
-const ProductList = () => {
+const Select=styled.select`
+padding: 10px;
+margin-right:10px;
+`
+const Option=styled.option``
+
+export default function ProductList() {
   return (
     <Container>
-        <Announcement/>
-        <Navbar/>
-        <Title>Dresses</Title>
-        <FilterContainer>
-            <Filter><FilterText>Filter Product</FilterText></Filter>
-            <Filter><FilterText>Sort Product</FilterText></Filter>
-        </FilterContainer>
-        <Products/>
-        <Newsletter/>
-        <Footer/>
+      <Navbar/>
+      <Anouncement/>
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products: </FilterText>
+          <Select>
+            <Option disabled selected>Color</Option>
+            <Option>White</Option>
+            <Option>Red</Option>
+            <Option>Black</Option>
+            <Option>Yellow</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>Size</Option>
+            <Option>Small</Option>
+            <Option>Medium</Option>
+            <Option>Large</Option>
+            <Option>Extra Large</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products</FilterText>
+          <Select>
+            <Option disabled selected> Newest</Option>
+            <Option>Price (Desc)</Option>
+            <Option>Price (Asc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products/>
+      <NewsLetter/>
+      <Footer/>
     </Container>
   )
 }
-
-export default ProductList
