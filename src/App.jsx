5532c1acbdce3328789f9d9be521298ffe4681ee
Biswajit Ghosh <ghoshbiswajit2406@gmail.com>
2022-08-17 21:11:55 +0530

    All Pages And Componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Home from "./Pages/Home"
 import ProductList from "./Pages/ProductList";
 import Product from "./Pages/Product";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Cart from "./Pages/Cart";
 
 const App = () => {
-  return <Product/>;
+  return <Cart/>;
 };
 
 export default App;
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+    rgba(255, 255, 255, 0.5),
+    rgba(255, 255, 255, 0.5)
+  ),
+    url("https://images.pexels.com/photos/4968390/pexels-photo-4968390.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+const BtnContainer= styled.div`
+    display:flex;
+    width: 100%;
+    justify-content:center;
+`
+
+const Login = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>SIGN IN</Title>
+                <Form>
+                    <Input placeholder="username" />
+                    <Input placeholder="password" />
+                    <BtnContainer>
+                    <Button>LOGIN</Button>
+                    </BtnContainer>
+                    <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+                    <Link>CREATE A NEW ACCOUNT</Link>
+                </Form>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Login;
