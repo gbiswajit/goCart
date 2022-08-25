2029c312b3092fff1631bb922849935ee38706e5
Biswajit Ghosh <ghoshbiswajit2406@gmail.com>
2022-08-25 19:25:14 +0530

    Updation using JWT

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User")
 const CryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken")
 //Register
 router.post("/register", async (req, res) => {
     const newUser = new User({
@@ -42,8 +43,14 @@ router.post('/login', async (req, res) => {
             res.status(401).json("Wrong Password");
             return
         }
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        }, process.env.JWT_SEC, { expiresIn: "3d" })
+
         const { password, ...others } = user._doc;
-        res.status(500).json(others)
+        res.status(500).json({ ...others, accessToken })
     } catch (err) {
         res.status(500).json(err)
     }
